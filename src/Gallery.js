c69fd064505f01d1d435d97383596074fe91b22c
scatterbrain3 <scattini3@hotmail.com>
2019-11-15 22:12:31 +1030

    Added Modal Images

--- src/Gallery.js
+++ src/Gallery.js
@@ -33,32 +33,66 @@ class Gallery extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            filter: ""
+            filter: "",
+            modal: false,
+            modalImg: ""
         }
         this.imgFilter = this.imgFilter.bind(this);
+        this.modal = this.modal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
 
     imgFilter(e){
         this.setState({
             filter: e.target.id
         })
-       console.log(this.state.filter)
     }
 
+    modal(e){
+        console.log(e.target.src)
+        this.setState({
+            modal: true,
+            modalImg: e.target.src
+        })
+    }
+
+    closeModal(){
+        this.setState({
+            modal: false
+        })
+    }
 
   
     render(){
-        let filterVal = this.state.filter
+        let filterVal = this.state.filter;
+        let modal = this.state.modal;
+        let modalUrl = this.state.modalImg;
+        let closeModal = this.closeModal;
+        function modalCheck(){
+            if(modal === true){
+                console.log(modalUrl);
+                return(
+                    <div className="modal">
+                        <img className="modalImg" src={modalUrl} />
+                        <button onClick={closeModal}>Close</button>
+                    </div>
+                )
+            }
+        }
         return(
             <div>
                 <Sidebar onClick={this.imgFilter} />
+                {modalCheck()}
                 <div className="Gallery-container">
-                    {this.props.photoObjectArray.filter(function(i){ if(filterVal !== "All" && filterVal !== ""){ return i.className === filterVal}else{return i
-
+                    {
+                    this.props.photoObjectArray.filter(function(i){ 
+                        if(filterVal !== "All" && filterVal !== ""){ 
+                            return i.className === filterVal}
+                        else{
+                            return i
                     } }).map(i=>{
-                        console.log(i.className, filterVal)
                         return(
-                            <div className="Gallery-Photo-container">
+                            <div onClick={this.modal} className="Gallery-Photo-container">
                                 <img src={i.src}  />
                             </div>
                         )
