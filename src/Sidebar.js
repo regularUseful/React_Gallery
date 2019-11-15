e36135999d92a8440389f29b28278a6dae3e2c57
scatterbrain3 <scattini3@hotmail.com>
2019-11-15 21:09:23 +1030

    Made responsive

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,7 +12,7 @@ class Sidebar extends React.Component{
                 <h1 className="Sidebar-heading"><span className="Sidebar-heading-1">Random</span><br />Gallery</h1>
                 <ol className="Sidebar-list">
                     {this.props.filters.map(i=>{
-                        return <li class="Sidebar-list-item" onClick={this.props.onClick} id={i}>{i}</li>
+                        return <li className="Sidebar-list-item" onClick={this.props.onClick} id={i}>{i}</li>
                     })}
                 </ol>
                 <p className="Sidebar-text">
